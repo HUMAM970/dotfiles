ca1020146d3f4c6f4aa0b55e85c17bda513d2523
HUMAM970 <humam.102001@gmail.com>
2025-12-12 03:53:19 +0300

    changed

--- .config/helix/scripts/yazi.fish
+++ .config/helix/scripts/yazi.fish
@@ -1,7 +1,20 @@
 #!/usr/bin/env fish
 
+set path $argv[1]
+
+if test -z "$path"
+    set path $PWD
+end
+
 set tmpfile (mktemp)
-yazi --chooser-file $tmpfile
-set selected (cat $tmpfile)
-rm $tmpfile
-echo $selected
+yazi --chooser-file "$tmpfile" "$path"
+set selected (cat "$tmpfile")
+rm "$tmpfile"
+
+set rel (string replace -r "^$PWD/?" "" -- $selected)
+
+if test -n "$rel" -a -f "$rel"
+    echo $rel
+else
+    echo "\"\""
+end
